d1dbc0ee8ec33e8067dfba53a90752d97029d82e
bolblechi <cardo.tw@gmail.com>
2024-10-08 21:43:13 -0600

    Addin usgs api file

--- src/api.tsx
+++ src/api.tsx
@@ -1,8 +1,7 @@
-import axios, { AxiosResponse, AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
+import axios, { AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
 import {useState, useEffect} from "react";
 
-export default function Earthquakes() {
-  const [response, setResponse] = useState<AxiosResponse>();
+// export default function Earthquakes() {
   const client = axios.create({
     baseURL: 'https://earthquake.usgs.gov',
   });
@@ -11,14 +10,7 @@ export default function Earthquakes() {
       'Authorization': `Bearer ${process.env.REACT_APP_USGS_API_KEY}`
     } as RawAxiosRequestHeaders,
   };
+  const getEarthquakesInfo = client.get(`/earthquakes/feed/v1.0/summary/all_day.geojson`, config);
 
-  useEffect(() => {
-    client.get(`/earthquakes/feed/v1.0/summary/all_day.geojson`, config).then((response) => {
-      setResponse(response.data);
-    });
-  }, []);
-
-  return (
-    response
-  );
-};
+export default getEarthquakesInfo;
+// };
