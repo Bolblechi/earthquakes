7ed31a49238a705dd1c45a1d042e7810c8be858a
bolblechi <cardo.tw@gmail.com>
2024-10-08 21:44:01 -0600

    Adding map component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Status, Wrapper } from '@googlemaps/react-wrapper';
+import MapComponent from './Components/Map';
 
 function App() {
+  const render = (status: Status) => (<h1>{status}</h1>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper apiKey={`${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`} render={render}>
+        <MapComponent />
+      </Wrapper>
     </div>
   );
 }
---
+++ src/Components/Map.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useRef, useState } from "react";
+import axios, { AxiosResponse} from 'axios';
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import getEarthquakesInfo from "../api"
+
+export default function MapComponent(){
+  const [map, setMap] = useState<google.maps.Map>()
+  const ref = useRef<HTMLDivElement>()
+  const [response, setResponse] = useState<AxiosResponse>();
+  const [markerCluster, setMarkerClusters] = useState<MarkerClusterer>();
+
+  useEffect(()=>{
+    if(ref.current && !map){
+      setMap(new window.google.maps.Map(ref.current, {
+        center: {lat: 4.4333479181711075, lng:-75.21505129646759},
+        zoom: 2,
+      }))
+    }
+  }, [map])
+
+  useEffect(() => {
+    if(!response && markerCluster){
+      getEarthquakesInfo.then((response) => {
+        setResponse(response);
+      });
+    }
+    if(markerCluster && response){
+      let markersNumber = response.data['features'].length
+      markerCluster.clearMarkers();
+
+      for (let i = 0; i < markersNumber; i++) {
+        markerCluster.addMarker(new window.google.maps.Marker({
+          position: {lat: response.data['features'][i].geometry.coordinates[1],
+                     lng: response.data['features'][i].geometry.coordinates[0]}
+        }))
+      }
+    }
+  }, [markerCluster, response]);
+
+  useEffect(() => {
+    if(map && !markerCluster){
+      setMarkerClusters(new MarkerClusterer({map, markers: [], }));
+    }
+  }, [map, markerCluster]);
+
+  if(response){
+    console.log(response.data['features'])
+  }
+
+  return (
+    <>
+      <div ref={ref as any} style={{height: "100%", width: "100%", minHeight:"700px"}} ></div>
+    </>
+  )
+}
